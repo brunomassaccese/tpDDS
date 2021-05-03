cf0f4be09305053e744e17426e7f0838e3c5a204
Santiagocast <santiagojcastelli@gmail.com>
2021-05-03 16:32:06 -0300

    Mejoras en el codigo

--- src/main/java/Validador/ValidadorDePass.java
+++ src/main/java/Validador/ValidadorDePass.java
@@ -2,24 +2,23 @@ package Validador;
 
 import java.io.*;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 
 public class ValidadorDePass {
 
     //Path para el archivo ordenado
-    private static String pathSinModificar = "src/main/java/Validador/10k-worst-passwords.txt";
-    private static String pathModificado = "src/main/java/Validador/sorted-10k-worst-passwords.txt";
+    private static final String pathSinModificar = "src/main/java/Validador/10k-worst-passwords.txt";
+    private static final String pathModificado = "src/main/java/Validador/sorted-10k-worst-passwords.txt";
     //Declaro y defino las variables que uso en el array
-    private static int LINES = 10000;
+    private static final int LINES = 10000;
     public static String[] strArray = new String[LINES];
 
     //Funcion principal validadora de pass
     public Boolean validar(String pass) throws IOException {
-        Boolean digito =false;
-        Boolean letter=false;
-        Boolean longitud = false;
-        Boolean enLista = false;
+        Boolean digito;
+        Boolean letter;
+        Boolean longitud;
+        Boolean enLista;
         enLista = passEnLista(pass);
         longitud = validarLongitud(pass);
         digito = validarDigitos(pass);
@@ -35,7 +34,7 @@ public class ValidadorDePass {
 
         File archivo;
         archivo = new File (path);
-        FileReader fr = null;
+        FileReader fr;
         BufferedReader br;
         fr = new FileReader (archivo);
         br = new BufferedReader(fr);
@@ -50,9 +49,7 @@ public class ValidadorDePass {
             i++;
             //System.out.println(linea);
         }
-        if( fr != null ){
             fr.close();
-        }
     }
 
     private static void arrayToFile (String [] array) {  //Pasa array ordenado a archivo nuevo
@@ -102,23 +99,17 @@ public class ValidadorDePass {
             fileToArray(pathModificado);
 
 
-        if (Arrays.binarySearch(strArray, pass)>0)
-            return true;
-        else
-            return false;
+        return Arrays.binarySearch(strArray, pass) > 0;
     }
 
     //VALIDAR QUE LA LONGITUD SEA > 8
     public Boolean validarLongitud(String pass){
-        if (pass.length() <8)
-            return false;
-        else
-            return true;
+        return pass.length() >= 8;
     }
 
     //VALIDAR QUE LA PASS TENGA DIGITOS
     public Boolean validarDigitos(String pass){
-        for (Integer i=0;i<=pass.length()-1;i++){
+        for (int i = 0; i<=pass.length()-1; i++){
             if(Character.isDigit(pass.charAt(i)))
                 return true;
         }
@@ -127,7 +118,7 @@ public class ValidadorDePass {
 
     //VALIDAR QUE LA PASS TENGA LETRAS
     public Boolean validarLetras (String pass) {  //REPITO CODIGO, solo habria que cambiar el isDigit por el isLetter
-        for (Integer i = 0; i <= pass.length()-1; i++) {
+        for (int i = 0; i <= pass.length()-1; i++) {
            if (Character.isLetter(pass.charAt(i)))
                return true;
         }
