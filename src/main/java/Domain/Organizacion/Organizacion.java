4acce77160fab7d2e676d32c4438208a58aa6911
Ariel <amarin@frba.utn.edu.ar>
2021-07-06 18:39:08 -0300

    XD

--- src/main/java/Domain/Organizacion/Organizacion.java
+++ src/main/java/Domain/Organizacion/Organizacion.java
@@ -9,3 +9,52 @@ public class Organizacion {
         caracteristicas.add(nuevaCaracteristica);
     }
 }
+
+/*
+import java.util.ArrayList;
+
+public class Organizacion {
+
+    public String nombre;
+    public ArrayList<String> preguntas;
+    public ArrayList<Publicacion> publicacionesPendientes;
+    public ArrayList<Publicacion> publicacionesAceptadas;
+
+    public Organizacion(){
+        this.preguntas = new ArrayList<>();
+        this.publicacionesPendientes = new ArrayList<>();
+        this.publicacionesAceptadas = new ArrayList<>();
+    }
+
+    public void agregarPublicacion(ArrayList<Publicacion> publicaciones, Publicacion publicacion){ publicaciones.add(publicacion); } //AGREGA A UNA DE LAS LISTAS SIRVE PARA EL PUNTO 1
+
+    public void agregarPregunta(String pregunta) { preguntas.add(pregunta); } //LA ORGANIZACION PUEDE AGREGAR SUS PREGUNTAS PUNTO 2
+
+    public void verTodasLasPreguntas(){ for (String s : preguntas) System.out.println(s); }
+
+    public int cantidadDePreguntas(){ return preguntas.size(); }
+
+    public void modificarPregunta(int lugar,String nuevaPregunta){ preguntas.set(lugar,nuevaPregunta); } // LA ORGANIZACION PUEDE MODIFICAR LA PREGUNTA PERO NO LA PUBLICACION OSEA LA PUBLICACION SE GUARDA CON LAS RESPUESTAS Y PREGUNTAS ANTERIORES NO VA MODIFICAR A LAS PREGUNTAS DE LAS PUBLICACIONES QUE FUERON CONTESTADAS ANTES PUNTO 2
+
+    public void verPublicaciones(ArrayList<Publicacion> publicaciones){
+
+        for (Publicacion publicacion : publicaciones) {
+
+            System.out.println(publicacion.usuario.nombre);
+            System.out.println(publicacion.mascota.nombre);
+
+            for(int i=0;i<publicacion.cantidad(publicacion.preguntas) ;i++){
+                System.out.println(i+1 + ")" + publicacion.preguntas.get(i));
+                System.out.println(publicacion.respuestas.get(i));
+            }
+        }
+    }
+
+    //PUNTO 3 ACA HAY Q SEGUIR CON LA PARTE DE NOTIFICAR A LA PERSONA YO QUERIA MANDAR ESTE MENSAJE AL USUARIO
+    //queria guardarlo como una lista de mensajes para el usuario no se si se puede
+    public void notificar (Publicacion publicacion, Usuario usuarioInteresado){
+        System.out.println("Nos comunicamos con usted para decirle que el usuario "+ usuarioInteresado.nombre + "esta interesado en adoptar a " + publicacion.mascota.nombre);
+    }
+}
+
+*/
--- src/main/java/Domain/Persona/Usuario.java
+++ src/main/java/Domain/Persona/Usuario.java
@@ -17,4 +17,67 @@ public class Usuario extends Persona implements Strategy {
 
 }
 
+/*
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Usuario {
+
+    public String nombre;
+
+    public void generarPublicacionDeAdopcion(Organizacion organizacion,Mascota mascota){ //PUNTO 1 GENERA UNA PUBLICACION
+
+        Publicacion publicacion = new Publicacion(); //INICIALIZA LA PUBLICACION PUNTO 1
+        publicacion.usuario = this; //ASIGNA PUNTO 1
+        publicacion.organizacion = organizacion;
+        publicacion.mascota = mascota;
+
+        this.responderPreguntas(publicacion);//RESPONDE PREGUNTAS PUNTO 1
+        publicacion.enviar();//ENVIA PASA A LA CLASE PUBLICACION PUNTO 1
+    }
+
+    public void responderPreguntas(Publicacion publicacion){ //PUNTO 1
+
+        ArrayList<String> preguntasDeLaOrganizacion = publicacion.organizacion.preguntas; //PARA NO REPETIR
+        int cantidad = publicacion.organizacion.cantidadDePreguntas();
+        Scanner respuesta = new Scanner (System.in); //PARA GUARDAR LAS RESPUESTAS
+        System.out.println("Responda todas las preguntas");
+
+        for(int i=0; i<cantidad ;i++){
+            System.out.println(i+1 + ")" + preguntasDeLaOrganizacion.get(i)); //MUESTRA LA RESPUESTA DE LA ORGANIZACION
+            publicacion.agregar(publicacion.preguntas, preguntasDeLaOrganizacion.get(i)); //GUARDA LA PREGUNTA EN LA PUBLICACION
+            publicacion.agregar(publicacion.respuestas,respuesta.nextLine()); // GUARDA LA RESPUESTA EN LA PUBLICACION
+        }
+    }
+
+    public void verPublicaciones(Organizacion organizacion){ //PUNTO 3 UN USUARIO PUEDE VER LAS PUBLICACION DE UNA ORGANIZACION Y SI VE UNA EN LA CUAL ESTA INTERESADO PUEDE ESCOGERLA
+
+        organizacion.verPublicaciones(organizacion.publicacionesAceptadas);
+        Scanner indicar = new Scanner(System.in);
+        Scanner indicarLugar = new Scanner(System.in);
+        int opcion;
+
+        do{
+            System.out.println("\n Opciones");
+            System.out.println("[1] Interesado en una publicacion");
+            System.out.println("[2] No interesado en ninguna publicacion");
+            opcion = indicar.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.println("Indicar el lugar de la publicacion");
+                    this.interesado(indicarLugar.nextInt(), organizacion);
+                    break;
+                case  2:
+                    System.out.println("Gracias vuelva pronto");
+                    break;
+            }
+        }while (opcion!=1 || opcion!=2);
+    }
+
+    public void interesado (int posicion, Organizacion organizacion){ // PUNTO 3 ESCOGE UNA PUBLICACION Y ESTA SE LE ENVIA A LA PUBLICACION LA CUAL LA AGREGARA A SU LISTA DE INTERESADOS
+        System.out.println("Muchas Gracias");
+        organizacion.publicacionesAceptadas.get(posicion).agregarUsuarioInteresado(this);
+    }
+
+}
+*/
