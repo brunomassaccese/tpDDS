dba1b90625fe140e3b5e30cf75930b593db6f529
Bruno Massaccese <bmassaccese@frba.utn.edu.ar>
2021-11-23 12:48:40 -0300

    Modificación persistence

--- src/main/java/Domain/entities/Mascota/Foto.java
+++ src/main/java/Domain/entities/Mascota/Foto.java
@@ -1,6 +1,7 @@
 package Domain.entities.Mascota;
 
 import javax.persistence.*;
+import java.sql.Blob;
 
 @Entity
 @Table(name = "foto")
@@ -9,19 +10,23 @@ public class Foto  {
     @GeneratedValue
     private int id;
 
-    @Column(name = "path")
-    private String path;
+    //@Column(name = "path")
+    //private String path;
+    @Column(name = "image")
+    private Blob image = null;
 
     @ManyToOne
     @JoinColumn(name = "mascota_id", referencedColumnName = "id")
     private Mascota mascota;
 
-    public Foto(String path) {
-        this.standarizar(path);
-        this.path = path;
+    public Foto(Blob image) {
+        this.standarizar(image);
+        //this.path = path;
+        this.image = image;
+
     }
 
-    private void standarizar(String path){
+    private void standarizar(Blob image){
         //TODO
     }
 
